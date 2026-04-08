abbaa65b6cf6660483214a27dc1508828a896887
m.talib <m.talib@ddc5-l-3301nb8.dir.svc.accenture.com>
2026-04-08 16:23:44 +0530

    First Commit

--- src/day4/Question2.java
+++ src/day4/Question2.java
@@ -5,16 +5,24 @@ package day4;
 public class Question2 {
 
 	public static void main(String[] args) {
-		int i=1;
-		while(i<=50) {
-			if(i%2==0) {
-				System.out.println(i);
-			}
-		i++;
-		}
+//		int i=1;
+//		while(i<=50) {
+//			if(i%2==0) {
+//				System.out.println(i);
+//			}
+//		i++;
+//		}
+		
+		
+		
+//Print 10...1
 
-		
-		
+		int i=10;
+		while(i>=1) {System.out.println(i);i--;
+
+		}    															
 	}
-
 }
+
+
+
