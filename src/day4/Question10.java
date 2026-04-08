c66d23e37d17b9c8a48e66b8f5937fa4afd7839a
m.talib <#SmTajSzpkh@123>
2026-04-09 04:04:33 +0530

    Night commit

--- src/day4/Question10.java
+++ src/day4/Question10.java
@@ -1,12 +1,32 @@
 package day4;
 
+import java.util.Scanner;
+
+// Display this AP ---->   1,3,5,7,9...n
 public class Question10 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		System.out.println("Hi Saksham");
+//        Scanner Sc=new Scanner(System.in);
+//        System.out.println("Select the End-Point of AP  :");
+//        int n=Sc.nextInt();
+//        for (int i=1;i<=n;i++){
+//            if (i%2!=0){
+//                System.out.println(i);
+//            }
+//        }
 
+
+// Find the highest factor of a number 'n' (other than itself)     70
+
+        Scanner Input=new Scanner(System.in);
+        System.out.println("Enter the number  :");
+        int x = Input.nextInt();
+        int HF=1;
+        for (int i=1; i<=x;i++){
+            if (x%i==0)
+                HF=i;
+        }
+        System.out.println(HF);
 	}
 
 }
